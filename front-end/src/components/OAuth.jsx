3fc13d1e57f166709ee29ed943cb20cafab81251
Sasithavinda4 <sasavind2021@gmail.com>
2023-12-07 15:42:57 +0530

    prevent the auto submission of the OAuth,

--- front-end/src/components/OAuth.jsx
+++ front-end/src/components/OAuth.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function OAuth() {
   return (
-    <button className='bg-red-700 p-3 rounded-lg uppercase text-white hover:opacity-80'>Continue with google</button>
+    <button type='button' 
+        className='bg-red-700 p-3 rounded-lg uppercase text-white
+        hover:opacity-80'>Continue with google</button>
   )
 }
